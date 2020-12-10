d9f2c670e069792bd9584251de779766b2935438
Matheus Tavares <matheus.tavares@mam.contab>
2020-12-10 15:40:15 -0300

    Updating commit

--- pages/jsObjects/jsObjects.js
+++ pages/jsObjects/jsObjects.js
@@ -127,3 +127,103 @@ Object.freeze(obj) //Congela objetos para não serem alterados
 
 //-----------------------------------------------------
 //Herança
+//Toda função tem um Objeto prototype, se não for especificado fica o padrão (root) que é o Object.prototype
+
+console.log(obj.__proto__ === Object.prototype) //
+
+const avo = {attr1: 'A'}
+const pai = {__proto__:avo, attr2: 'B'}
+const filho = {__proto__:pai, attr3: 'C'}
+
+console.log(filho.attr1) //Nesse caso, como filho não tem attr1, busca no pai, depois no avô. Se avô não tivesse, buscaria no Object.protorype.
+
+
+//Exemplo
+
+const carzin = {
+  velMax: 200,
+  velAtual: 0,
+  acelera(delta){
+    if(this.velAtual + delta <= this.velMax){
+      this.velAtual += delta
+    }else{
+      this.velAtual = this.velMax
+    }
+  },
+
+  status() {
+    return `${this.velAtual}Km/h de ${this.velMax}Km/h `
+  }
+}
+
+const ferrari = {
+  modelo: 'F40',
+  velMax: 350, //Shadowing - Quando o atributo sombreia o atributo do prototype. (usa o da objeto criado e não o do modelo)
+}
+
+const hyundai = {
+  modelo: 'hb20',
+
+  status() {
+    return `${this.modelo}: ${super.status()}` //O super. pega as informações do prototype acima.
+  },
+}
+
+Object.setPrototypeOf(ferrari,carzin) //Seta o prototype de ferrari como carro (ferrari é filha de carro)
+Object.setPrototypeOf(hyundai,carzin)
+
+console.log(ferrari)
+
+ferrari.acelera(100)
+console.log(ferrari.status())
+
+console.log(hyundai)
+
+hyundai.acelera(200)
+console.log(hyundai.status())
+
+//Object.create - Pega como base o pai e já seta o prototype ao invés de ter que setar depois
+const father = {cabelo: "preto", nome: "Joao"}
+
+const daugther1 = Object.create(father)
+daugther1.nome = "Maria"
+
+console.log(daugther1.nome)
+console.log(daugther1.cabelo)
+
+const daugther2 = Object.create(pai,{
+  nome: {value: "Bia", writable: false, enumerable: true}
+})
+
+console.log(daugther2.nome)
+
+console.log(Object.keys(daugther1))
+console.log(Object.keys(daugther2))
+
+for (let keys in daugther2){
+  daugther2.hasOwnProperty(keys) ? console.log(keys) : console.log(`Por herança: ${keys}`)
+}
+
+//Todas os objetos criados a partir da mesma função construtora tem o mesmo prototype.
+
+function myObject() {}
+
+console.log(myObject.__proto__)
+
+const ob1 = new myObject()
+const ob2 = new myObject()
+
+console.log(ob1.__proto__, ob2.__proto__)
+
+// -------------------------------------------------------
+
+//Para evitar modificações na hora de criar o objeto podemos usar as seguintes funções:
+//Object.preventExtensions() - Não deixa criar novos atributos, porem permite modificar ou excluir os existentes
+//Object.Seal() - Sela o objeto, não podendo excluir, modificar ou atribuir novos parametros
+
+// -------------------------------------------------------
+//JSON vs Objetos
+
+console.log(JSON.stringify(carzin))
+console.log(JSON.parse('{"velMax":200,"velAtual":0}'))
+
