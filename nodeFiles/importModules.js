cc4230c067424e8fe6e7f4c01a5be64370382fff
Matheus Tavares <matheus.tavares@mam.contab>
2020-12-14 09:26:40 -0300

    created a new topic: Node

---
+++ nodeFiles/importModules.js
@@ -0,0 +1,10 @@
+const nodeModuleA = require('./nodeModuleA')
+const nodeModuleB = require('./nodeModuleB')
+
+console.log(nodeModuleA.ola)
+console.log(nodeModuleA.bemVindo)
+console.log(nodeModuleA.ateLogo)
+console.log(nodeModuleA)
+  
+console.log(nodeModuleB.bomDia)
+console.log(nodeModuleB.boaNoite)
---
+++ nodeFiles/nodeModuleA.js
@@ -0,0 +1,5 @@
+//Exportando algo de dentro do módulo node para ser usado em outro arquivo
+
+this.ola = "Fala Galera!"
+exports.bemVindo = "Bem vindo ao Node"
+module.exports.ateLogo = "Até logo!"
