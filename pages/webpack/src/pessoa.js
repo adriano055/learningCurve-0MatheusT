666e8f83b52c791e9aebca696d00bbc2afd6792d
Matheus Tavares <matheus.tavares@mam.contab>
2020-12-28 09:33:53 -0300

    new topic - webpack

---
+++ pages/webpack/src/pessoa.js
@@ -0,0 +1,7 @@
+export default class Pessoa {
+  cumprimentar() {
+    return "olá"
+  }
+}
+
+//utilizando padrões de modo do ecma script, juntamente com a class.
---
+++ pages/webpack/webpack.config.js
@@ -0,0 +1,6 @@
+const webpack = require('webpack') //padrão node
+
+module.exports = {
+  mode: 'development',
+  entry: './src/principal.js'
+}
