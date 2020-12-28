6165635f2a0c64cc14d84c5c5b274ec1aaa5211e
Matheus Tavares <matheus.tavares@mam.contab>
2020-12-28 11:41:47 -0300

    finalized webpack topic

--- pages/webpack/webpack.config.js
+++ pages/webpack/webpack.config.js
@@ -1,6 +1,54 @@
-const webpack = require('webpack') //padrão node
+const modoDev = process.env.NODE_ENV !== 'production'
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
-  mode: 'development',
-  entry: './src/principal.js'
-}
+  mode: modoDev ? 'development' : 'production', //modo de desenvolvimento, produção...
+  entry: './src/principal.js', //entrada de arquivos
+  output: {                    //Condigurações de saída.
+    filename: 'principal.js',
+    path: __dirname + '/public' //defino o caminho.
+  },
+  devServer: {
+    contentBase: "./public",
+    port: 9000
+  },
+  optimization: {
+      minimizer: [
+          new UglifyJsPlugin({
+              cache: true,
+              parallel: true
+          }),
+          new OptimizeCSSAssetsPlugin({})
+      ]
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "estilo.css"
+    })
+  ],
+  
+
+  module: {     //cadastrando o loader para reconhecer o css.
+    rules: [
+      {
+        //test: /\.css$/,  //expressão regular parar ler .css
+        test: /\.s?[ac]ss$/,
+        //test: /\.s?[ac]css$/,  //expressão regular parar ler sass
+        use: [
+          MiniCssExtractPlugin.loader,    //Transforma o css para externo (Conflita com o style-loader)
+          //'style-loader', //responsável por adc dentro da dom a tag <style>
+          'css-loader', //interpreta imports, urls dentro do css.
+          'sass-loader'
+        ]
+      }, {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: ['file-loader']
+      }]
+    }
+  }
+
+//no final, tudo fica em um único arquivo js.
